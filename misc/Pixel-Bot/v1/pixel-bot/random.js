23153c407e292d9986d9078c39dfd2855917b875
jniac <joseph.merdrignac@gmail.com>
2018-11-09 15:02:06 +0100

    better PixelBot.random() options

--- misc/Pixel-Bot/v1/pixel-bot/random.js
+++ misc/Pixel-Bot/v1/pixel-bot/random.js
@@ -1,5 +1,7 @@
 
-export default (...args) => {
+const randomMinMax = (min, max) => min + (max - min) * Math.random()
+
+const random = (...args) => {
 
     if (args.length === 1) {
 
@@ -12,6 +14,18 @@ export default (...args) => {
         } else if (typeof arg === 'number') {
 
             return arg * Math.random()
+
+        } else if (typeof arg === 'object') {
+
+            let { min = 0, max = 1, type = 'float' } = arg
+
+            if (type === 'int') {
+
+                return Math.floor(randomMinMax(min, max))
+
+            }
+
+            return randomMinMax(min, max)
 
         }
 
@@ -26,3 +40,5 @@ export default (...args) => {
     throw `random.js Error, can not interpret arguments: ${args}`
 
 }
+
+export default random
