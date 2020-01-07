f4e1a80e1a57f642e2ce8dfa94a9806c3a04c956
jniac <joseph.merdrignac@gmail.com>
2020-01-07 01:34:12 +0100

    [taaabs]

--- javascript/threejs/start-threejs/scripts/Vertigo.js
+++ javascript/threejs/start-threejs/scripts/Vertigo.js
@@ -1,112 +1,112 @@
 
 class Vertigo extends THREE.Object3D {
 
-    constructor(perspective = 1, aspect = 1) {
+	constructor(perspective = 1, aspect = 1) {
 
-        super()
+		super()
 
-        // allow a much more natural manipulation
-        this.rotation.order = 'YXZ'
+		// allow a much more natural manipulation
+		this.rotation.order = 'YXZ'
 
-        const toRad = Math.PI / 180
+		const toRad = Math.PI / 180
 
-        let width = 12
-        let fovRef = 45
+		let width = 12
+		let fovRef = 45
 		let fov = fovRef
 
-        let perspectiveCamera = new THREE.PerspectiveCamera(fov, aspect)
-        let orthographicCamera = new THREE.OrthographicCamera(-width / 2, width / 2, -width / aspect / 2, width / aspect / 2)
+		let perspectiveCamera = new THREE.PerspectiveCamera(fov, aspect)
+		let orthographicCamera = new THREE.OrthographicCamera(-width / 2, width / 2, -width / aspect / 2, width / aspect / 2)
 
-        this.add(orthographicCamera)
-        this.add(perspectiveCamera)
+		this.add(orthographicCamera)
+		this.add(perspectiveCamera)
 
-        let isOrthographic = fov < 1e-4
+		let isOrthographic = fov < 1e-4
 
-        const compute = () => {
+		const compute = () => {
 
-            let height = width / aspect
+			let height = width / aspect
 
-            if (isOrthographic = fov < 1e-4) {
+			if (isOrthographic = fov < 1e-4) {
 
-                let w = width / 2
-                let h = height / 2
-                orthographicCamera.left = -w
-                orthographicCamera.right = w
-                orthographicCamera.bottom = -h
-                orthographicCamera.top = h
-                orthographicCamera.position.set(0, 0, width)
-                orthographicCamera.updateProjectionMatrix()
+				let w = width / 2
+				let h = height / 2
+				orthographicCamera.left = -w
+				orthographicCamera.right = w
+				orthographicCamera.bottom = -h
+				orthographicCamera.top = h
+				orthographicCamera.position.set(0, 0, width)
+				orthographicCamera.updateProjectionMatrix()
 
-            } else {
+			} else {
 
-                let d = (height / 2) / Math.tan(fov * toRad / 2)
-                perspectiveCamera.fov = fov
-                perspectiveCamera.aspect = aspect
-                perspectiveCamera.position.set(0, 0, d)
-                perspectiveCamera.updateProjectionMatrix()
+				let d = (height / 2) / Math.tan(fov * toRad / 2)
+				perspectiveCamera.fov = fov
+				perspectiveCamera.aspect = aspect
+				perspectiveCamera.position.set(0, 0, d)
+				perspectiveCamera.updateProjectionMatrix()
 
-            }
+			}
 
-        }
+		}
 
-        const setPerspective = (value) => {
+		const setPerspective = (value) => {
 
-            perspective = value
+			perspective = value
 			fov = fovRef * perspective
 
-            compute()
+			compute()
 
-        }
+		}
 
-        setPerspective(perspective)
+		setPerspective(perspective)
 
-        Object.defineProperties(this, {
+		Object.defineProperties(this, {
 
-            perspective: {
-                get: () => perspective,
-                set: setPerspective,
-            },
+			perspective: {
+				get: () => perspective,
+				set: setPerspective,
+			},
 
-            width: {
-                get: () => width,
-                set: value => {
-                    width = value
-                    compute()
-                }
-            },
+			width: {
+				get: () => width,
+				set: value => {
+					width = value
+					compute()
+				}
+			},
 
-            height: {
-                get: () => width / aspect,
-                set: value => {
-                    width = value * aspect
-                    compute()
-                }
-            },
+			height: {
+				get: () => width / aspect,
+				set: value => {
+					width = value * aspect
+					compute()
+				}
+			},
 
-            aspect: {
-                get: () => aspect,
-                set: value => {
-                    aspect = value
-                    compute()
-                }
-            },
+			aspect: {
+				get: () => aspect,
+				set: value => {
+					aspect = value
+					compute()
+				}
+			},
 
-            isOrthographic: {
-                get:() => isOrthographic,
-            },
+			isOrthographic: {
+				get:() => isOrthographic,
+			},
 
-            camera: {
-                get: () => isOrthographic ? orthographicCamera : perspectiveCamera
-            }
+			camera: {
+				get: () => isOrthographic ? orthographicCamera : perspectiveCamera
+			}
 
-        })
+		})
 
-        Object.assign(this, {
+		Object.assign(this, {
 
-            orthographicCamera,
-            perspectiveCamera,
+			orthographicCamera,
+			perspectiveCamera,
 
-        })
-    }
+		})
+	}
 
 }
