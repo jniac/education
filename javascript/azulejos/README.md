05dcdb492ce6497636eb066eacf070b3fafbae60
jniac <joseph.merdrignac@gmail.com>
2020-01-23 16:59:42 +0100

    readme + starter

--- javascript/azulejos/README.md
+++ javascript/azulejos/README.md
@@ -1,5 +1,22 @@
 # Azulejo
 
-[wikipédia](https://en.wikipedia.org/wiki/Azulejo)
+## Brief exercice — Design génératif
 
-![image](./azulejos.jpg)
+A partir du fichier [starter.zip](./starter.zip), réalisez une déclinaison de la grille de sorte que les images produites soit originales.
+
+Les moyens pour y parvenir sont :
+- le design des [primitives (svg)](./starter/assets) via n'importe quel logiciel (illustrator, figma etc.)
+  <div style="display: flex; width: 100%">
+	<img src="starter/assets/bigstar.svg">
+	<img src="starter/assets/circle.svg">
+	<img src="starter/assets/four.svg">
+	<img src="starter/assets/redline-3.svg">
+  </div>
+- le code javascript [(main.js)](./starter/main.js#L56-L71), par la modification des règles qui commandent l'ajout de nouvelle `<div class="layer"\>`.
+  Il s'agit ici de contrôler l'apparition des images en ajustant la probabilité d'apparition, ex:
+  - `if (Math.random() < 0.9) {...}`: très fréquent (90% de chance d'apparaître)
+  - `if (Math.random() < 0.1) {...}`: rare (10% de chance d'apparaître)
+
+<image src='./azulejos.jpg' width='500'>
+
+[Azulejo sur wikipédia](https://en.wikipedia.org/wiki/Azulejo)
---
+++ javascript/azulejos/starter/main.js
@@ -0,0 +1,77 @@
+
+let rotateTile = (element) => {
+
+	// alt + shift + L > |
+	// altGr + ?? > |
+	let a = (element.a || 0) + 90
+	element.a = a
+	element.style.transform = 'rotate(' + a + 'deg)'
+
+}
+
+let rotateRandomTile = () => {
+
+	let tiles = document.querySelectorAll('section#s2 div.tile')
+	let index = Math.floor(Math.random() * tiles.length)
+	let randomTile = tiles[index]
+	rotateTile(randomTile)
+
+}
+
+setInterval(rotateRandomTile, 3000)
+
+
+
+
+
+
+let makeGrid = (col, row, tileSize) => {
+
+	let container = document.querySelector('section#s3 div.grid')
+	let total = col * row
+
+	container.style.width = (tileSize * col) + 'px'
+	container.style.height = (tileSize * row) + 'px'
+
+	for (let i = 0; i < total; i++) {
+
+		let div = document.createElement('div')
+		container.append(div)
+
+		div.style.width = tileSize + 'px'
+		div.style.height = tileSize + 'px'
+
+		// mettre une couleur en arrière plan ?
+		// div.style.background = ['red', 'blue', '#fc0', 'purple', '#09f'][i % 5]
+
+		let appendImage = (url) => {
+
+			let layer = document.createElement('div')
+			div.append(layer)
+			layer.classList.add('layer')
+			layer.style.backgroundImage = `url(${url})`
+
+		}
+
+		// le design génératif commence ici:
+		if (Math.random() < 0.2) {
+			appendImage('assets/circle.svg')
+		}
+
+		if (Math.random() < 0.5) {
+			appendImage('assets/bigstar.svg')
+		}
+
+		if (Math.random() < 0.1) {
+			appendImage('assets/four.svg')
+		}
+
+		if (Math.random() < 0.5) {
+			appendImage('assets/redline-3.svg')
+		}
+
+	}
+
+}
+
+makeGrid(8, 6, 80)
